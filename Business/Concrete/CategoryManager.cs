df6527c4324d10017c2ac7d62b182230c5fa748b
Alperen Dİler <alperendiler1@gmail.com>
2023-11-13 01:45:05 +0300

    category

--- Business/Concrete/CategoryManager.cs
+++ Business/Concrete/CategoryManager.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,19 @@ using System.Threading.Tasks;
 
 namespace Business.Concrete
 {
-    public class CategoryManager
+    public class CategoryManager : ICategoryService
     {
+        ICategoryDal _categoryDal;
+
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
+        public List<Category> GetAll()
+        {
+            return _categoryDal.GetAll();
+        }
+
+       
     }
 }
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,14 +1,31 @@
 ﻿using Business.Concrete;
 using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
 
 ProductManager productManager = new ProductManager(new EfProductDal());
+CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+
+
+foreach (var category in categoryManager.GetAll())
+
+{
+    Console.Write(category.CategoryId + "-) ");
+
+
+    Console.WriteLine(category.CategoryName);
+
+
+}
+
 
 foreach (var product in productManager.GetAllByCategoryId(1))
 
 {
     Console.WriteLine(product.ProductName);
     Console.WriteLine(product.UnitPrice);
+    Console.WriteLine(product.CategoryId);
+
 
 }
 
