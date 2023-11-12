f95b4b38fe317cf5813c5c7284cdc9ba3b13285c
Alperen Dİler <alperendiler1@gmail.com>
2023-11-13 01:29:12 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,28 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+
+ProductManager productManager = new ProductManager(new EfProductDal());
+
+foreach (var product in productManager.GetAllByCategoryId(1))
+
+{
+    Console.WriteLine(product.ProductName);
+    Console.WriteLine(product.UnitPrice);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
